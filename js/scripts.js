b2ea37f665d37ccdb35a48e02e1a990231a85c88
Alisson <alisson.paulino@aluno.unifapce.edu.br>
2023-03-01 18:12:57 -0300

    Teste de par ou ímpar v1

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function start() {
+    var form = document.getElementById("formulario");
+    var captura = form.numero.value;
+    var test = captura % 2;
+    if (test == 0){
+        form.resposta.value = "Número Par";
+    } else {
+        form.resposta.value = "Número Impar";
+    }
+}
